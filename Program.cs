e5e58d7bd22055f6741a6734ad78e9eb17a5fef4
Svetlana <korneevdenis.ru@mail.ru>
2022-11-07 18:06:09 +0300

    задание 1

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите трехзначное  число");
+int number = int.Parse(Console.ReadLine()!);
+
+int IsSecondDigit(int number)
+{
+    int SecondDigit = number / 10 % 10;
+    return SecondDigit;
+}
+
+int  res = IsSecondDigit(number);
+Console.WriteLine(res);
